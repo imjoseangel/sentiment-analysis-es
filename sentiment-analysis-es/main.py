8362b8e4eb52704f7178c5555d7fd962d331110e
imjoseangel <josea.munoz@gmail.com>
2021-07-18 10:54:39 +0200

    basic structure

---
+++ sentiment-analysis-es/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
