3e169c02a374f5d194dbd05c200dfcf8dd908147
imjoseangel <josea.munoz@gmail.com>
2021-07-19 23:07:04 +0200

    add base

--- sentiment-analysis-es/main.py
+++ sentiment-analysis-es/main.py
@@ -5,16 +5,13 @@ from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
 
 import pandas as pd
-import numpy as np
-from pandas.core.indexes import category
+from sklearn.naive_bayes import MultinomialNB
 
 
 def main():
     # Read the data from CSV file
     sentiments = pd.read_csv('data/sentiments.csv', sep=',')
     emotions = pd.read_csv('data/emotions.csv', sep=',')
-    print(sentiments.head())
-    print(emotions.head())
 
 
 if __name__ == '__main__':
