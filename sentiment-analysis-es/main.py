d53b29369aadf053a8e45233da73068662707be9
imjoseangel <josea.munoz@gmail.com>
2021-07-25 23:05:21 +0200

    add classification

--- sentiment-analysis-es/main.py
+++ sentiment-analysis-es/main.py
@@ -3,6 +3,7 @@
 
 from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
+from numpy import negative
 
 import pandas as pd
 from sklearn.naive_bayes import MultinomialNB
@@ -12,6 +13,12 @@ def main():
     # Read the data from CSV file
     sentiments = pd.read_csv('data/sentiments.csv', sep=',')
     emotions = pd.read_csv('data/emotions.csv', sep=',')
+
+    positive = sentiments[sentiments['polarity'] == 'positive']
+    negative = sentiments[sentiments['polarity'] == 'negative']
+
+    print(positive)
+    print(negative)
 
 
 if __name__ == '__main__':
