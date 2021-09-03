f7ab8121e42b77ee2329e4abe2d94a8393aeab99
imjoseangel <josea.munoz@gmail.com>
2021-09-03 11:09:53 +0200

    add stopwords

--- sentiment-analysis-es/main.py
+++ sentiment-analysis-es/main.py
@@ -3,14 +3,27 @@
 
 from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
+from sklearn.naive_bayes import MultinomialNB
+import re
+import pandas as pd
+
+from nltk.stem.porter import *
 from numpy import negative
 
-import pandas as pd
-from sklearn.naive_bayes import MultinomialNB
-import matplotlib.pyplot as plt
+
+def stem_words(words: list) -> list:
+
+    stemmer = PorterStemmer()
+    return stemmer.stem(words)
+
+
+def break_into_words(words: list) -> list:
+
+    return re.findall("[a-zA-Z0-9]+", words)
 
 
 def main():
+
     # Read the data from CSV file
     sentiments = pd.read_csv('data/sentiments.csv', sep=',')
     emotions = pd.read_csv('data/emotions.csv', sep=',')
@@ -18,6 +31,10 @@ def main():
     positive = sentiments[sentiments['polarity'] == 'positive']
     negative = sentiments[sentiments['polarity'] == 'negative']
     neutral = sentiments[sentiments['polarity'] == 'neutral']
+
+    positivelst = (positive['word'].tolist())
+    negativelst = (negative['word'].tolist())
+    neutrallst = (neutral['word'].tolist())
 
 
 if __name__ == '__main__':
