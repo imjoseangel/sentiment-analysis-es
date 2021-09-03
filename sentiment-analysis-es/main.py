45af306a49f074e58ce28ab19f2c9610a5ff5792
imjoseangel <josea.munoz@gmail.com>
2021-09-03 11:35:43 +0200

    add training base

--- sentiment-analysis-es/main.py
+++ sentiment-analysis-es/main.py
@@ -3,12 +3,19 @@
 
 from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
+
+import nltk
 from sklearn.naive_bayes import MultinomialNB
 import re
 import pandas as pd
 
 from nltk.stem.porter import *
 from numpy import negative
+
+
+def to_lower(words: list) -> list:
+
+    return words.lower()
 
 
 def stem_words(words: list) -> list:
@@ -22,11 +29,16 @@ def break_into_words(words: list) -> list:
     return re.findall("[a-zA-Z0-9]+", words)
 
 
+def skip_numeric(words: list) -> list:
+    return [word for word in words if not word.isnumeric()]
+
+
 def main():
 
     # Read the data from CSV file
     sentiments = pd.read_csv('data/sentiments.csv', sep=',')
     emotions = pd.read_csv('data/emotions.csv', sep=',')
+    train = pd.read_csv('data/train.csv', sep=',')
 
     positive = sentiments[sentiments['polarity'] == 'positive']
     negative = sentiments[sentiments['polarity'] == 'negative']
@@ -35,6 +47,9 @@ def main():
     positivelst = (positive['word'].tolist())
     negativelst = (negative['word'].tolist())
     neutrallst = (neutral['word'].tolist())
+
+    print(train['sentence'].map(to_lower).map(
+        break_into_words).map(skip_numeric))
 
 
 if __name__ == '__main__':
